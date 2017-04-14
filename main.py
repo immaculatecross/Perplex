9e62f341931921000e70e53a7621b9dfc809e052
Centroyd <marclundwall@Marcs-MacBook-Air.local>
2017-04-15 00:23:26 +0200

    New functions, infinite loop, hidden password and more!
    Password(name,masterkey,website) give you instantly a string of the
    password for these three elements
    
    while True:
    makes the loop infinite, so every time you type a website, it gives you
    the password and then immediately prompts for another website
    //we can change this later
    
    with get pass, the password input and length is hidden from the screen
    to protect from prying eyes :)

--- main.py
+++ main.py
@@ -1,77 +1,9 @@
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+import getpass
 
 
-# input data
-
-name= raw_input ('Enter your full name: ')
-masterkey= raw_input ('Enter your master key: ')
-website= raw_input ('Enter the name of the sevice you want to store the password of: ')
-
-
-# hash input data
-
-m = SHA256.new()
-m.update(name)
-name=m.hexdigest()
-
-m = SHA256.new()
-m.update(masterkey)
-masterkey=m.hexdigest()
-
-m = SHA256.new()
-m.update(website)
-website=m.hexdigest()
-
-
-# fix input data hash length (multiple of 16 for plaintext, 32 for key), by adding "0" or truncating
-
-while len(name)%16 != 0:
-    name+="0"
-
-if len(masterkey)>31:
-    masterkey=masterkey[0:32]
-
-while len(masterkey)<32:
-    masterkey+="0"
-
-
-# encrypt plain with key and a constant IV ("thisisaninitvect"), then hash ciph1
-
-
-obj1=AES.new(masterkey, AES.MODE_CBC, 'thisisaninitvect')
-ciph1 = obj1.encrypt(name)
-
-m = SHA256.new()
-m.update(ciph1)
-ciph1=m.hexdigest()
-
-
-# fix hash length (multiple of 16 for plaintext, exactly 32 for key), by adding "0" or truncating
-
-while len(website)%16 != 0:
-    website+="0"
-
-if len(ciph1)>31:
-    ciph1=ciph1[0:32]
-
-while len(ciph1)<32:
-    ciph1+="0"
-
-
-# encrypt website with ciph1 and a constant IV ("thisisaninitvect"), then hash ciph2
-
-obj=AES.new(ciph1, AES.MODE_CBC, 'thisisaninitvect')
-ciph2=obj.encrypt(website)
-
-m = SHA256.new()
-m.update(ciph2)
-ciph2=m.hexdigest()
-
-
-# convert ciph2 from hexadecimal to decimal, then from decimal to base62 (def from Stack Overflow, credit to Mark Borgerding)
-
-ciph2 = int(ciph2, 16)
+# convert from decimal to base 62 (def from Stack Overflow, credit to Mark Borgerding)
 
 def int2base(x,b,alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
     'convert an integer to its string representation in a given base'
@@ -94,14 +26,8 @@ def int2base(x,b,alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
         rets = alphabet[idx] + rets
     return rets
 
-ciph2 = int2base(ciph2,62,alphabet='0123456789abcdefghijklmnopqrstuvwxyz')
 
-
-# check to see if cipher2's sequences contains at least one number, one upper-case letter, and one lower-case letter, and choose the correct interval
-
-digit = 0
-upper = 0
-lower = 0
+# check to see if cipher2's sequences contains at least one number, one upper-case letter, and one lower-case letter
 
 def Works (string, constant):
     digit = 0
@@ -120,23 +46,111 @@ def Works (string, constant):
     else:
         return False
 
-for i in range (0,6):
-    if Works (ciph2,i):
-        c = i*12
-        break
-    elif i == 5:
-        c=0
-        if digit == 0:
-            ciph2 = ciph2[0:2]+"0"+ciph2[3:12]
-        if upper == 0:
-            ciph2 = "P"+ciph2[1:12]
-        if lower == 0:
-            ciph2 = ciph2[0:1]+"w"+ciph2[2:12]
-        break
+
+# return the password from the name, the masterkey and the website
+
+def Password (name, masterkey, website):
+
+    # hash input data
+
+    m = SHA256.new()
+    m.update(name)
+    name=m.hexdigest()
+
+    m = SHA256.new()
+    m.update(masterkey)
+    masterkey=m.hexdigest()
+
+    m = SHA256.new()
+    m.update(website)
+    website=m.hexdigest()
 
 
-# use the following password format: xxx-xxx-xxx-xxx, where x is a base 62 caracter, from the start of ciph2
+    # fix input data hash length (multiple of 16 for plaintext, 32 for key), by adding "0" or truncating
 
-password = ciph2[0+c:3+c]+"-"+ciph2[3+c:6+c]+"-"+ciph2[6+c:9+c]+"-"+ciph2[9+c:12+c]
+    while len(name)%16 != 0:
+        name+="0"
 
-print password
+    if len(masterkey)>31:
+        masterkey=masterkey[0:32]
+
+    while len(masterkey)<32:
+        masterkey+="0"
+
+
+    # encrypt plain with key and a constant IV ("thisisaninitvect"), then hash ciph1
+
+
+    obj1=AES.new(masterkey, AES.MODE_CBC, 'thisisaninitvect')
+    ciph1 = obj1.encrypt(name)
+
+    m = SHA256.new()
+    m.update(ciph1)
+    ciph1=m.hexdigest()
+
+
+    # fix hash length (multiple of 16 for plaintext, exactly 32 for key), by adding "0" or truncating
+
+    while len(website)%16 != 0:
+        website+="0"
+
+    if len(ciph1)>31:
+        ciph1=ciph1[0:32]
+
+    while len(ciph1)<32:
+        ciph1+="0"
+
+
+    # encrypt website with ciph1 and a constant IV ("thisisaninitvect"), then hash ciph2
+
+    obj=AES.new(ciph1, AES.MODE_CBC, 'thisisaninitvect')
+    ciph2=obj.encrypt(website)
+
+    m = SHA256.new()
+    m.update(ciph2)
+    ciph2=m.hexdigest()
+
+
+    # convert ciph2 from hexadecimal to decimal, then from decimal to base62
+
+    ciph2 = int(ciph2, 16)
+
+    ciph2 = int2base(ciph2,62,alphabet='0123456789abcdefghijklmnopqrstuvwxyz')
+
+
+    # Choose the correct interval
+
+    digit = 0
+    upper = 0
+    lower = 0
+
+    for i in range (0,6):
+        if Works (ciph2,i):
+            c = i*12
+            break
+        elif i == 5:
+            c=0
+            if digit == 0:
+                ciph2 = ciph2[0:2]+"0"+ciph2[3:12]
+            if upper == 0:
+                ciph2 = "P"+ciph2[1:12]
+            if lower == 0:
+                ciph2 = ciph2[0:1]+"w"+ciph2[2:12]
+            break
+
+
+    # use the following password format: xxx-xxx-xxx-xxx, where x is a base 62 caracter, from the start of ciph2
+
+    password = ciph2[0+c:3+c]+"-"+ciph2[3+c:6+c]+"-"+ciph2[6+c:9+c]+"-"+ciph2[9+c:12+c]
+
+    return password
+
+
+# input data
+
+name= raw_input ('Enter your full name: ')
+masterkey= getpass.getpass ('Enter your master key: ')
+
+while True:
+    website= raw_input ('Enter the service domain name: ')
+    print "Your password is:", Password (name, masterkey, website)
