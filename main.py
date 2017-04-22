da6a08758d4455235b9e59795d27a6992d3c2d41
Centroyd <marclundwall@Marcs-MacBook-Air.local>
2017-04-22 14:51:58 +0200

    Lots of changes

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+import codecs
 import getpass
-
-
-# convert from decimal to base 62 (def from Stack Overflow, credit to Mark Borgerding)
-
-def int2base(x,b,alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
-    'convert an integer to its string representation in a given base'
-    if b<2 or b>len(alphabet):
-        if b==62: # assume base64 rather than raise error
-            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-        else:
-            raise AssertionError("int2base base out of range")
-    if isinstance(x,complex): # return a tuple
-        return ( int2base(x.real,b,alphabet) , int2base(x.imag,b,alphabet) )
-    if x<=0:
-        if x==0:
-            return alphabet[0]
-        else:
-            return  '-' + int2base(-x,b,alphabet)
-    # else x is non-negative real
-    rets=''
-    while x>0:
-        x,idx = divmod(x,b)
-        rets = alphabet[idx] + rets
-    return rets
 
 
 # check to see if cipher2's sequences contains at least one number, one upper-case letter, and one lower-case letter
@@ -51,71 +27,44 @@ def Works (string):
 
 def Password (name, masterkey, website):
 
-    # hash input data
+
+    # combine and hash into ciph
+
+    ciph = name + masterkey + website
 
     m = SHA256.new()
-    m.update(name)
-    name=m.hexdigest()
-
-    m = SHA256.new()
-    m.update(masterkey)
-    masterkey=m.hexdigest()
-
-    m = SHA256.new()
-    m.update(website)
-    website=m.hexdigest()
+    m.update(ciph)
+    ciph=m.hexdigest()
+    #print ciph
 
 
-    # fix key hash length (must be 32), moreover, plaintext must be a multiple of 16, but a SHA256 hash is always 64 character long
+    # convert ciph from hexadecimal to base64
 
-    masterkey=masterkey[0:32]
-
-
-    # encrypt plain with key and a constant IV ("thisisaninitvect"), then hash ciph1
-
-    obj1=AES.new(masterkey, AES.MODE_CBC, 'thisisaninitvect')
-    ciph1 = obj1.encrypt(name)
-
-    m = SHA256.new()
-    m.update(ciph1)
-    ciph1=m.hexdigest()
+    ciph = codecs.encode(codecs.decode(ciph, 'hex'), 'base64').decode()
+    ciph = ciph.replace("+", "")
+    ciph = ciph.replace("/", "")
+    #print ciph
+    #print len(ciph)
 
 
-    # fix key hash length (must be 32), moreover, plaintext must be a multiple of 16, but a SHA256 hash is always 64 character long
+    # hash and convert for as long as ciph doesn't meet requirements
 
-    ciph1=ciph1[0:32]
-
-
-    # encrypt website with ciph1 and a constant IV ("thisisaninitvect"), then hash ciph2
-
-    obj=AES.new(ciph1, AES.MODE_CBC, 'thisisaninitvect')
-    ciph2=obj.encrypt(website)
-
-    m = SHA256.new()
-    m.update(ciph2)
-    ciph2=m.hexdigest()
-
-
-    # convert ciph2 from hexadecimal to decimal, then from decimal to base62
-
-    ciph2 = int(ciph2, 16)
-
-    ciph2 = int2base(ciph2,62,alphabet='0123456789abcdefghijklmnopqrstuvwxyz')
-
-
-    # hash and convert for as long as ciph2 doesn't meet requirements
-
-    while Works(ciph2) == False:
+    while Works(ciph[0:12]) == False:
+        #print ciph
+        #print len(ciph)
         m = SHA256.new()
-        m.update(ciph2)
-        ciph2=m.hexdigest()
-        ciph2 = int(ciph2, 16)
-        ciph2 = int2base(ciph2,62,alphabet='0123456789abcdefghijklmnopqrstuvwxyz')
+        m.update(ciph)
+        ciph=m.hexdigest()
+        #print ciph
+        ciph = codecs.encode(codecs.decode(ciph, 'hex'), 'base64').decode()
+        ciph = ciph.replace("+", "")
+        ciph = ciph.replace("/", "")
+        #print ciph
 
 
-    # use the following password format: xxx-xxx-xxx-xxx, where x is a base 62 caracter, from the start of ciph2
+    # use the following password format: xxx-xxx-xxx-xxx, where x is a base 62 caracter, from the start of ciph
 
-    password = ciph2[0:3]+"-"+ciph2[3:6]+"-"+ciph2[6:9]+"-"+ciph2[9:12]
+    password = ciph[0:3]+"-"+ciph[3:6]+"-"+ciph[6:9]+"-"+ciph[9:12]
 
     return password
 
