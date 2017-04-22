664eb98fbadcc64a5ec30e148f77a9535d8a32a6
immaculatecross <watermelon8239@gmail.com>
2017-04-22 18:34:01 +0200

    huhp

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from Crypto.Hash import SHA256
 import codecs
 import getpass
 
+# encrypt with SHA-256, turn into base 64, and remove unwanted characters
+def Encrypt (string):
+    # hash using SHA-256
+    m = SHA256.new()
+    m.update(string)
+    string=m.hexdigest()
+    # turn into base 64
+    string = codecs.encode(codecs.decode(string, 'hex'), 'base64').decode()
+    # remove unwanted characters
+    string = string.replace("+", "")
+    string = string.replace("/", "")
+    return string
 
 # check to see if cipher2's sequences contains at least one number, one upper-case letter, and one lower-case letter
-
 def Works (string):
     digit = 0
     upper = 0
@@ -22,58 +33,23 @@ def Works (string):
     else:
         return False
 
-
 # return the password from the name, the masterkey and the website
-
 def Password (name, masterkey, website):
-
-
-    # combine and hash into ciph
-
+    # combine input data into ciph
     ciph = name + masterkey + website
-
-    m = SHA256.new()
-    m.update(ciph)
-    ciph=m.hexdigest()
-    #print ciph
-
-
-    # convert ciph from hexadecimal to base64
-
-    ciph = codecs.encode(codecs.decode(ciph, 'hex'), 'base64').decode()
-    ciph = ciph.replace("+", "")
-    ciph = ciph.replace("/", "")
-    #print ciph
-    #print len(ciph)
-
-
+    # encrypt with SHA-256, turn into base 64, and remove unwanted characters
+    ciph = Encrypt (ciph)
     # hash and convert for as long as ciph doesn't meet requirements
-
-    while Works(ciph[0:12]) == False:
-        #print ciph
-        #print len(ciph)
-        m = SHA256.new()
-        m.update(ciph)
-        ciph=m.hexdigest()
-        #print ciph
-        ciph = codecs.encode(codecs.decode(ciph, 'hex'), 'base64').decode()
-        ciph = ciph.replace("+", "")
-        ciph = ciph.replace("/", "")
-        #print ciph
-
-
+    while Works(ciph) == False:
+        ciph = Encrypt (ciph)
     # use the following password format: xxx-xxx-xxx-xxx, where x is a base 62 caracter, from the start of ciph
-
     password = ciph[0:3]+"-"+ciph[3:6]+"-"+ciph[6:9]+"-"+ciph[9:12]
-
     return password
 
-
 # input data
-
 name= raw_input ('Enter your full name: ')
 masterkey= getpass.getpass ('Enter your master key: ')
-
+# infinite loop to prompt for website
 while True:
     website= raw_input ('Enter the service domain name: ')
     print "Your password is:", Password (name, masterkey, website)
